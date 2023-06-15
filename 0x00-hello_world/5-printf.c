0dc0d1696f3f5ce1673bf107cc361dad68e20d23
Kondi-ma <elongakondima@gmail.com>
2023-06-15 15:05:28 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio>
+/**
+ * main - Line printed using printf function
+ * Return: 0 (Success
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
