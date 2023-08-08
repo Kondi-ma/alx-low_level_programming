c3e5b1a336d6781024ffe063813c2cac1e899a1c
Kondi-ma <elongakondima@gmail.com>
2023-08-07 19:40:48 -0700

    update1

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,19 +11,19 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int i, j, len = 0;
+	int i, j, blues = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len];)
-			len++;
+		for (blues = 0; text_content[blues];)
+			blues++;
 	}
 
 	i = open(filename, O_WRONLY | O_APPEND);
-	j = write(i, text_content, len);
+	j = write(i, text_content, blues);
 
 	if (i == -1 || j == -1)
 		return (-1);
